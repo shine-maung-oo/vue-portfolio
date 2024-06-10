353634c274ab4bd6d0450774a4371df4716200aa
shine-maung-oo <shinemaungoo@hmmserver.net>
2024-06-10 23:07:37 +0630

    update 404 page

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
   <article class="active">
     <header>
-      <h2 class="h2 article-titl">Page Not Found</h2>
+      <h2 class="h2 article-title">Page Not Found</h2>
     </header>
 
     <section class="about-text mt-5">
@@ -9,11 +9,14 @@
       <p>
         The Page you are looking for doesn't exist or an other error occured. 😞
       </p>
-      <button class="form-btn ms-0" @click="router.back()">Go back</button>
+      <button class="form-btn ms-0" @click="back()">Go back</button>
     </section>
   </article>
 </template>
 <script setup>
 import { useRouter } from "vue-router";
 const router = useRouter();
+const back = () => {
+  router.push({'name': 'home'})
+}
 </script>
